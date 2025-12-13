4102d4880648deb36e3db569bb241d045cf51964
Javier Arias <103268935+JavierAriasCol@users.noreply.github.com>
2025-12-12 22:12:57 -0500

    refactor: remove Resize, Copy, and Save options from image menu

--- flutter_quill_extensions/lib/src/editor/image/image_menu.dart
+++ flutter_quill_extensions/lib/src/editor/image/image_menu.dart
@@ -1,19 +1,11 @@
-import 'package:flutter/cupertino.dart' show showCupertinoModalPopup;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart'
-    show ImageUrl, QuillController, StyleAttribute, getEmbedNode;
+    show QuillController, getEmbedNode;
 import 'package:flutter_quill/internal.dart';
-import 'package:path/path.dart' as p;
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../common/utils/element_utils/element_utils.dart';
-import '../../common/utils/string.dart';
 import 'config/image_config.dart';
-import 'image_load_utils.dart';
-import 'image_save_utils.dart';
-import 'widgets/image.dart' show ImageTapWrapper, getImageStyleString;
-import 'widgets/image_resizer.dart' show ImageResizer;
+import 'widgets/image.dart' show ImageTapWrapper;
 
 class ImageOptionsMenu extends StatelessWidget {
   const ImageOptionsMenu({
@@ -23,7 +15,6 @@ class ImageOptionsMenu extends StatelessWidget {
     required this.imageSize,
     required this.readOnly,
     required this.imageProvider,
-    this.prefersGallerySave = true,
     super.key,
   });
 
@@ -34,19 +25,6 @@ class ImageOptionsMenu extends StatelessWidget {
   final bool readOnly;
   final ImageProvider imageProvider;
 
-  // TODO(quill_native_bridge): Update this doc comment once saveImageToGallery()
-  //  is supported on Windows too (will be applicable like macOS). See https://pub.dev/packages/quill_native_bridge#-features
-  /// Determines if the image should be saved to the gallery instead of using the
-  /// system file save dialog for platforms that support both.
-  ///
-  /// Currently, the only platform where this applies is macOS.
-  ///
-  /// This is silently ignored on platforms that only support gallery save (Android and iOS)
-  /// or only image save.
-  ///
-  /// For more details, refer to [quill_native_bridge Saving images](https://pub.dev/packages/quill_native_bridge#-saving-images).
-  final bool prefersGallerySave;
-
   @override
   Widget build(BuildContext context) {
     final materialTheme = Theme.of(context);
@@ -55,63 +33,6 @@ class ImageOptionsMenu extends StatelessWidget {
       child: SimpleDialog(
         title: Text(context.loc.image),
         children: [
-          if (!readOnly)
-            ListTile(
-              title: Text(context.loc.resize),
-              leading: const Icon(Icons.settings_outlined),
-              onTap: () {
-                Navigator.pop(context);
-                showCupertinoModalPopup<void>(
-                  context: context,
-                  builder: (modalContext) {
-                    final screenSize = MediaQuery.sizeOf(modalContext);
-                    return ImageResizer(
-                      onImageResize: (width, height) {
-                        final res = getEmbedNode(
-                          controller,
-                          controller.selection.start,
-                        );
-
-                        final attr = replaceStyleStringWithSize(
-                          getImageStyleString(controller),
-                          width: width,
-                          height: height,
-                        );
-                        controller
-                          ..skipRequestKeyboard = true
-                          ..formatText(
-                            res.offset,
-                            1,
-                            StyleAttribute(attr),
-                          );
-                      },
-                      imageWidth: imageSize.width,
-                      imageHeight: imageSize.height,
-                      maxWidth: screenSize.width,
-                      maxHeight: screenSize.height,
-                    );
-                  },
-                );
-              },
-            ),
-          ListTile(
-            leading: const Icon(Icons.copy_all_outlined),
-            title: Text(context.loc.copy),
-            onTap: () async {
-              Navigator.of(context).pop();
-              controller.copiedImageUrl = ImageUrl(
-                imageSource,
-                getImageStyleString(controller),
-              );
-
-              final imageBytes = await ImageLoader.instance
-                  .loadImageBytesFromImageProvider(
-                      imageProvider: imageProvider);
-              if (imageBytes != null) {
-                await ClipboardServiceProvider.instance.copyImage(imageBytes);
-              }
-            },
-          ),
           if (!readOnly)
             ListTile(
               leading: Icon(
@@ -142,89 +63,6 @@ class ImageOptionsMenu extends StatelessWidget {
                 await config.onImageRemovedCallback.call(imageSource);
               },
             ),
-          ListTile(
-            leading: const Icon(Icons.save),
-            title: Text(context.loc.save),
-            onTap: () async {
-              final messenger = ScaffoldMessenger.of(context);
-              final localizations = context.loc;
-              Navigator.of(context).pop();
-
-              SaveImageResult? result;
-              try {
-                result = await ImageSaver.instance.saveImage(
-                  imageUrl: imageSource,
-                  imageProvider: imageProvider,
-                  prefersGallerySave: prefersGallerySave,
-                );
-              } on GalleryImageSaveAccessDeniedException {
-                messenger.showSnackBar(SnackBar(
-                    content: Text(
-                  localizations.saveImagePermissionDenied,
-                )));
-                return;
-              }
-
-              if (result == null) {
-                messenger.showSnackBar(SnackBar(
-                    content: Text(
-                  localizations.errorUnexpectedSavingImage,
-                )));
-                return;
-              }
-
-              if (kIsWeb) {
-                messenger.showSnackBar(SnackBar(
-                    content: Text(localizations.successImageDownloaded)));
-                return;
-              }
-
-              if (result.isGallerySave) {
-                messenger.showSnackBar(SnackBar(
-                  content: Text(localizations.successImageSavedGallery),
-                  action: SnackBarAction(
-                    label: localizations.openGallery,
-                    onPressed: QuillNativeProvider.instance.openGalleryApp,
-                  ),
-                ));
-                return;
-              }
-
-              if (isDesktopApp) {
-                final imageFilePath = result.imageFilePath;
-                if (imageFilePath == null) {
-                  // User canceled the system save dialog.
-                  return;
-                }
-
-                messenger.showSnackBar(
-                  SnackBar(
-                    content: Text(localizations.successImageSaved),
-                    // On macOS the app only has access to the picked file from the system save
-                    // dialog and not the directory where it was saved.
-                    // Opening the directory of that file requires entitlements on macOS
-                    // See https://pub.dev/packages/url_launcher#macos-file-access-configuration
-                    // Open the saved image file instead of the directory
-                    action: defaultTargetPlatform == TargetPlatform.macOS
-                        ? SnackBarAction(
-                            label: localizations.openFile,
-                            onPressed: () => launchUrl(Uri.file(imageFilePath)),
-                          )
-                        : SnackBarAction(
-                            label: localizations.openFileLocation,
-                            onPressed: () => launchUrl(
-                                Uri.directory(p.dirname(imageFilePath))),
-                          ),
-                  ),
-                );
-
-                return;
-              }
-
-              throw StateError(
-                  'Image save result is not handled on $defaultTargetPlatform');
-            },
-          ),
           ListTile(
             leading: const Icon(Icons.zoom_in),
             title: Text(context.loc.zoom),
